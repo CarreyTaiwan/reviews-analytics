876cec5eb39c847eecccfc2620f699c54ba0261e
CarreyTaiwan <ylhzcjvcode@gmail.com>
2019-06-05 22:30:06 +0800

    calc aveg_len reviews

--- read.py
+++ read.py
@@ -4,10 +4,15 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1 # count = count + 1
-		if count % 10000 == 0:
+		if count % 100000 == 0:
 				print(len(data))
-print(len(data))
+print('檔案讀取完了, 總共有', len(data), '筆資料')
 
-print(data[0])
-print('-----------')
-print(data[1])
+sum_len = 0
+y = 0
+for d in data:
+	sum_len += len(d) 
+	
+print('留言的平均長度為', sum_len / len(data))
+
+
